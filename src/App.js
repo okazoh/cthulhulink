f3dae7e57a85a4f2b73aceb4987d8f3391c1ad80
Kazuo Itoh <itoh@cthulhu.link>
2017-12-08 18:27:37 +0900

    Redux組み込み＋Reducerを分離

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  render() {
+  render () {
+    if (this.props.SessionReducer.session == null) {
+      return (
+        <div className='App'>
+          <h1>ログイン処理</h1>
+          <input
+            type='button'
+            name='test'
+            value='test'
+            onClick={e => this.props.login('test', 'pass')} />
+        </div>
+      )
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+var mapStateToProps = (state) => {
+  return state
+}
+
+var mapDispatchToProps = (dispatch) => {
+  return {
+    login: (username, password) => {
+      alert('test')
+      dispatch({type: 'login',username: username, password: password})
+    },
+    logout: () => {
+      dispatch({type: 'logout'})
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const appInitialState = {
+  session: null
+}
+
+const SessionReducer = (state = appInitialState , action) => {
+  switch (action.type) {
+    case 'login':
+      alert('login')
+      return Object.assign({}, {session: {username: action.username,password: action.password}})
+    case 'logout':
+      return Object.assign({}, {session: null})
+    default:
+      return state
+  }
+}
+const RootReducer = combineReducers({SessionReducer})
+const store = createStore(RootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
